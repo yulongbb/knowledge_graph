635277612091eeb6a8e401b2934c6c68b05e45d8
yulongbb <sunyulongbb@gmail.com>
2024-06-21 16:44:44 +0800

    起始页

--- ui/src/main/home/home.component.ts
+++ ui/src/main/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { XPosition } from '@ng-nest/ui';
+import { XData, XSliderNode } from '@ng-nest/ui';
+import { Observable, map, tap } from 'rxjs';
+import { ImageService } from '../image/image.service';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +10,47 @@ import { XPosition } from '@ng-nest/ui';
 })
 export class HomeComponent implements OnInit {
 
+  keyword = '';
+  data: XData<XSliderNode> = ['目标库', '文库', '图库', '音频库', '视频库',];
+  size = 20;
+  index = 1;
+  data$!: Observable<any>;
 
+  constructor(
+    private service: ImageService,
 
-  constructor() { }
+  ) {
+    this.data$ = this.service
+      .getList(this.index, this.size, { collection: 'entity', keyword: `%${this.keyword}%` })
+      .pipe(
+        tap((x: any) => console.log(x)),
+        map((x: any) => x)
+      );
+  }
 
   ngOnInit(): void {
   }
+
+  search(keyword: any) {
+
+  }
+
+  getMd(row: any) {
+    console.log(row);
+    if (row.type.label == 'PDF') {
+      return 6;
+    }
+    if (row.type.label == '图像') {
+      return 8;
+    }
+    if (row.type.label == '音频') {
+      return 10;
+    }
+    if (row.type.label == '视频') {
+      return 12;
+    }
+    return 4;
+  }
+
 
 }
--- ui/src/main/home/home.module.ts
+++ ui/src/main/home/home.module.ts
@@ -3,16 +3,28 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 import { XDrawerModule } from '@ng-nest/ui/drawer';
-import { XButtonModule, XRadioModule } from '@ng-nest/ui';
+import { XButtonModule, XInputModule, XRadioModule } from '@ng-nest/ui';
+import { ShareModule } from 'src/share/share.module';
+import { XSliderModule } from '@ng-nest/ui/slider';
+import { XLayoutModule } from '@ng-nest/ui/layout';
+import { XCardModule } from '@ng-nest/ui/card';
+
+
+
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
+    ShareModule,
+    XLayoutModule,
     XDrawerModule,
     XButtonModule,
     XRadioModule,
+    XInputModule,
+    XSliderModule,
+    XCardModule,
     RouterModule.forChild([
       {
         path: '',
@@ -21,5 +33,6 @@ import { XButtonModule, XRadioModule } from '@ng-nest/ui';
     ]),
   ],
   exports: [RouterModule],
+
 })
 export class HomeModule {}
