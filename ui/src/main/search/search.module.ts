209e7e49ecb7ba9ece1116d70709ba83859c89ab
yulongbb <sunyulongbb@gmail.com>
2024-12-05 11:18:01 +0800

    文章正文

--- ui/src/main/search/search.module.ts
+++ ui/src/main/search/search.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { RouterModule } from '@angular/router';
 import { XDrawerModule } from '@ng-nest/ui/drawer';
-import { XAvatarModule, XButtonModule, XCheckboxModule, XInputModule, XLinkModule, XMenuModule, XRadioModule, XTagModule, XTimelineModule } from '@ng-nest/ui';
+import { XAvatarModule, XButtonModule, XCheckboxModule, XInputModule, XLinkModule, XMenuModule, XRadioModule, XTagModule, XTextareaModule, XTimelineModule } from '@ng-nest/ui';
 import { ShareModule } from 'src/share/share.module';
 import { XSliderModule } from '@ng-nest/ui/slider';
 import { XLayoutModule } from '@ng-nest/ui/layout';
@@ -27,6 +27,7 @@ import { XSelectModule } from '@ng-nest/ui/select';
     XButtonModule,
     XRadioModule,
     XInputModule,
+    XTextareaModule,
     XMenuModule,
     XSliderModule,
     XCardModule,
