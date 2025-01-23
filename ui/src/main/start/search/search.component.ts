cff0d55815a5c5e81118e572d6867da7d5c5627f
yulongbb <sunyulongbb@gmail.com>
2025-01-23 18:08:50 +0800

    提交

--- ui/src/main/start/search/search.component.ts
+++ ui/src/main/start/search/search.component.ts
@@ -257,7 +257,7 @@ export class SearchComponent implements OnInit {
             });
 
             item?._source?.videos?.forEach((video: any) => {
-              this.videos.push(video);
+              this.videos.push({...video,...item});
             });
           });
           this.ontologyService.get(data.list[0]?._source?.type).subscribe((t: any) => {
@@ -716,26 +716,19 @@ export class SearchComponent implements OnInit {
       .subscribe((data: any) => {
         data.list.forEach((item: any) => {
           item?._source?.images?.forEach((image: any) => {
-            if (
-              image.split('.')[image.split('.').length - 1] == 'jpg' ||
-              image.split('.')[image.split('.').length - 1] == 'jpeg' ||
-              image.split('.')[image.split('.').length - 1] == 'png' ||
-              image.split('.')[image.split('.').length - 1] == 'webp'
-            ) {
-              this.images.push({ _id: item._id, image: image, label: item?._source.labels.zh.value });
-            }
-            if (
-              image.split('.')[image.split('.').length - 1] == 'mp4'
-            ) {
-              this.videos.push({ _id: item._id, image: image, label: item?._source.labels.zh.value });
-            }
-
-            if (
-              image.split('.')[image.split('.').length - 1] == 'pdf'
-            ) {
-              this.pdfs.push({ _id: item._id, image: image, label: item?._source.labels.zh.value, description: item?._source.descriptions.zh.value });
-            }
+            this.images.push({ _id: item._id, image: image, label: item?._source.labels.zh.value });
           });
+          item?._source?.videos?.forEach((video: any) => {
+            this.videos.push({...video,...item});
+          });
+          item?._source?.pdfs?.forEach((pdf: any) => {
+            this.pdfs.push(pdf);
+          });
+          if (item._source.location) {
+            const newMarker = marker([item._source.location.lat, item._source.location.lon]);
+            this.markers.push(newMarker);
+          }
+          
           this.ontologyService.get(item._source.type).subscribe((t: any) => {
             item._type = t.label;
             this.ontologyService
