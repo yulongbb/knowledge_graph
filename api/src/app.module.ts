b82c52496cecb77026851277ff4eb2433c42d7d5
yulongbb <sunyulongbb@gmail.com>
2025-04-14 14:59:54 +0800

    删除冗余代码

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SystemModule } from './system/system.module';
-import { DesignModule } from './design/design.module';
 import { AuthModule } from './auth/auth.module';
 import { OntologynModule } from 'src/ontology/ontology.module';
 import { MinioClientModule } from './minio/minio-client.module';
 import { RedisModule } from './redis/redis.module';
 import { KnowledgeModule } from './knowledge/knowledge.module';
-import { DatasetModule } from './dataset/dataset.module';
-import { ChatModule } from './chat/chat.module';
-import { PluginModule } from './plugin/plugin.module';
 import { AddonModule } from './addons/addon.module';
-import { ProjectModule } from './project/project.module';
-import { GptManagementModule } from './gpt-management/gpt-management.module';
-import { MarkerModule } from './marker/marker.module';
-import { ThreeDModelModule } from './3d-model/3d-model.module';
 
 @Module({
   imports: [
@@ -30,20 +22,12 @@ import { ThreeDModelModule } from './3d-model/3d-model.module';
       logging: ['query', 'error'],
     }),
     SystemModule,
-    DesignModule,
     OntologynModule,
     KnowledgeModule,
-    DatasetModule,
     MinioClientModule,
     AuthModule,
     RedisModule,
-    ChatModule,
-    PluginModule,
     AddonModule,
-    ProjectModule,
-    GptManagementModule,
-    MarkerModule,
-    ThreeDModelModule,
   ],
   controllers: [],
   providers: [],
